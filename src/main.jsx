e82cb895e637f79aed6bed4e81a9ecc1123a3964
Mazen Mahmoud <bra74039@gmail.com>
2025-10-29 16:06:31 +0300

    fix importing in react-head

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from "react-head";
+import { HeadProvider } from "react-head";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
