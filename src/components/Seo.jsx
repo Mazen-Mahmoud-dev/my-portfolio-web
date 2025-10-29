2583cc423cf3b62a0934d281ff60daa3f58b7ccf
Mazen Mahmoud <bra74039@gmail.com>
2025-10-29 16:08:53 +0300

    fix issue in Seo.jsx removing unused Helmet for react-head

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -3,14 +3,12 @@ import { Title, Meta } from "react-head";
 const SEO = ({title,description}) => {
   return (
     <>
-      <Helmet>
         <Title>{title}</Title>
         <Meta
           name="description"
           content={description}
         />
         <link rel="canonical" href="https://www.mazenmahmoud.dev/" />
-      </Helmet>
     </>
   )
 }
